97758d566b4e36ebc6096406caa1a90d462f7608
Akhilesh yadav <akhileshyadav310596@gmail.com>
2024-03-26 21:53:18 +0530

    validate input

--- app/(admin)/menu/create.tsx
+++ app/(admin)/menu/create.tsx
@@ -1,22 +1,59 @@
 import Button from '@/components/Button'
-import React from 'react'
+import React, { useState } from 'react'
 import { StyleSheet, Text, TextInput, View } from 'react-native'
 
 function CreateProductScreen() {
+  const [name, setName] = useState('');
+  const [price, setPrice] = useState('');
+  const [error, setError] = useState('');
+
+  const resetFields = () => {
+    setName('');
+    setPrice('');
+  }
+
+  const validateInput = () => {
+    setError('');
+    if(!name){
+      setError('Name is required');
+      return false
+    }
+    if(!price){
+      setError('Price is required');
+      return false;
+    }
+    if(isNaN(parseFloat(price))){
+      setError('Price is not a number');
+      return false
+    }
+    return true;
+  }
 
   const onCreate = () => {
-    console.warn('Creating Product')
+    if(!validateInput()){
+      return;
+    }
+    console.warn('Creating Product',name,price);
+    resetFields();
   };
   return (
     <View style={styles.container}>
     <Text style={styles.label}>Name</Text>
-    <TextInput placeholder='Name' style={styles.input}/>
+    <TextInput
+     placeholder='Name' 
+     style={styles.input}
+     value={name}
+     onChangeText={setName}
+    />
     <Text style={styles.label}>Price ($)</Text>
     <TextInput 
       placeholder='9.99' 
       style={styles.input}
       keyboardType="numeric"
+      value={price}
+      onChangeText={setPrice}
       />
+      <Text style={{color:'red'}}>{error}</Text>
       <Button onPress={onCreate} text='Create'/>
     </View>
   )
