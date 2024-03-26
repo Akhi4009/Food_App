4027dd4e703f965ac9dd0f383a1bc3db1b19cb8e
Akhilesh yadav <akhileshyadav310596@gmail.com>
2024-03-26 09:15:56 +0530

    Admin side

--- app/(admin)/menu/[id].tsx
+++ app/(admin)/menu/[id].tsx
@@ -1,26 +1,12 @@
 import products from '@/assets/data/products';
-import Button from '@/components/Button';
-import { useCart } from '@/provider/CartProvider';
-import {  PizzaSize } from '@/types';
-import { useRouter } from 'expo-router';
 import { Stack, useLocalSearchParams } from 'expo-router';
-import { useState } from 'react';
-import { Image, Pressable, StyleSheet, Text, View } from 'react-native';
 
-const sizes:PizzaSize[] = ['S', 'M', 'L', 'XL'];
+import { Image, StyleSheet, Text, View } from 'react-native';
+
 const ProductDetailsScreen = () => {
-
-  const [selectedsize,setSelectedsize] = useState<PizzaSize>('M');
-  const { addItem } = useCart()
   const {id} = useLocalSearchParams();
-  const router = useRouter()
   const product = products.find((p) => p.id.toString() === id);
 
-  const addToCart = () => {
-      if(!product) return;
-      addItem(product,selectedsize)
-        router.push('/cart')
-  }
   
   if(!product) {
       return <Text> Product not found </Text>
@@ -29,27 +15,9 @@ const ProductDetailsScreen = () => {
     <View style={styles.container}>
       <Stack.Screen options={{title:product.name}}/>
       <Image source={{uri:product.image}} style={styles.image}/> 
-      
-      <Text>Select Sizes:</Text>
-      <View style={styles.sizes}>
-        {sizes.map(size =>
-        <Pressable
-          onPress={()=>{
-            setSelectedsize(size)
-          }}
-          key={size}
-          style={[styles.size,
-          {backgroundColor:selectedsize === size ? 'gainsboro' : 'white'}]}
-           >
-          <Text style={[styles.sizeText, {color: selectedsize === size ? 'black': 'gray'}]}>{size}</Text>
-        </Pressable> 
-          )}
-        
-      </View>
-      
+      <Text style={styles.title}>{product.name}</Text>
       <Text style={styles.price}>Price: ${product.price}</Text>
-      <Button   onPress={addToCart} text='Add to cart'/>
-    </View>
+     </View>
   )
 }
 
@@ -68,23 +36,9 @@ const styles = StyleSheet.create({
   price:{
     fontSize:18,
     fontWeight:'bold',
-    marginTop:'auto'
   },
-  sizes:{
-    flexDirection:'row',
-    justifyContent:'space-around',
-    marginVertical:10
-  },
-  size:{
-    backgroundColor:'gainsboro',
-    width:50,
-    aspectRatio:1,
-    borderRadius:25,
-    alignItems:'center',
-    justifyContent:'center'
-  },
-  sizeText:{
+  title:{
     fontSize:20,
-    fontWeight:'500'
   }
+ 
 })
--- app/(admin)/menu/_layout.tsx
+++ app/(admin)/menu/_layout.tsx
@@ -9,23 +9,43 @@ export default function MenuStack() {
     const colorScheme = useColorScheme();
     
     return (
-        <Stack screenOptions={{
-            headerRight: () => (
-                <Link href="/cart" asChild>
-                  <Pressable>
-                    {({ pressed }) => (
-                      <FontAwesome
-                        name="shopping-cart"
-                        size={25}
-                        color={Colors[colorScheme ?? 'light'].text}
-                        style={{ marginRight: 15, opacity: pressed ? 0.5 : 1 }}
-                      />
-                    )}
-                  </Pressable>
-                </Link>
-              ),
-        }}>
-            <Stack.Screen name="index" options={{title:'Menu'}}/>
+        <Stack screenOptions={{}}>
+            <Stack.Screen name="index" 
+              options={{
+                title:'Menu',
+                headerRight: () => (
+                  <Link href="/cart" asChild>
+                    <Pressable>
+                      {({ pressed }) => (
+                        <FontAwesome
+                          name="plus-square-o"
+                          size={25}
+                          color={Colors[colorScheme ?? 'light'].text}
+                          style={{ marginRight: 15, opacity: pressed ? 0.5 : 1 }}
+                        />
+                      )}
+                    </Pressable>
+                  </Link>
+                )
+                }}/>
+                <Stack.Screen name="[id]" 
+              options={{
+                title:'Menu',
+                headerRight: () => (
+                  <Link href="/cart" asChild>
+                    <Pressable>
+                      {({ pressed }) => (
+                        <FontAwesome
+                          name="pencil"
+                          size={25}
+                          color={Colors[colorScheme ?? 'light'].text}
+                          style={{ marginRight: 15, opacity: pressed ? 0.5 : 1 }}
+                        />
+                      )}
+                    </Pressable>
+                  </Link>
+                )
+                }}/>
         </Stack>
     )
 }
